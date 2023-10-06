f886cde8bbb4bc4bd4d140f696f6a2946afcf3f9
shahd-riad <shahd.riad2344@gmail.com>
2023-10-05 20:33:20 -0700

    Advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	char str[]= "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	for (int i = 0; i < sizeof(str); i++)
+	{
+		putchar(str[i]);
+	}
+	return (1);
+}
