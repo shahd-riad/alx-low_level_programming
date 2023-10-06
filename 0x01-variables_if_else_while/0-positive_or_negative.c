d2b8b5e068455a4ccb9b55fa50fdf5ef89437684
shahd-riad <shahd.riad2344@gmail.com>
2023-10-05 22:18:07 -0700

    f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,11 @@ int main(void)
 	/*to round it and center it around zero*/
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf(" %d is positive", n);
+	
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf(" %d is negative", n);
+		printf("%d is negative\n", n);
 	else if (n == 0)
-		printf(" %d is zero", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
