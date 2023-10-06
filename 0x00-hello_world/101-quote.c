978ef28bdc9145bf2e98658ee56fd57a48a2c79a
shahd-riad <shahd.riad2344@gmail.com>
2023-10-05 22:14:30 -0700

    LAST

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *    */
 int main(void)
 {
-	char str[]= "and that piece of art is useful" - Dora Korpar, 2015-10-19";
-	for (int i = 0; i < sizeof(str); i++)
-	{
-		putchar(str[i]);
-	}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
