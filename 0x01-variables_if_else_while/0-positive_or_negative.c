c5c077fce0b31294ef29686e49e3cc87a9c54c54
shahd-riad <shahd.riad2344@gmail.com>
2023-10-05 22:01:15 -0700

    BETTY

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,8 @@ int main(void)
 	if (n > 0) 
 		printf("%d is positive", n);
 	else if (n < 0)
-		printf(" %d is negative ", n);
+		printf("%d is negative", n);
 	else if (n == 0)
-		printf("%d is zero" ,n);
+		printf("%d is zero", n);
 	return (0);
 }
