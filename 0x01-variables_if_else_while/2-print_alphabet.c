58a6ddf0cf3a37e0c273130cc8929a1078ed96df
shahd-riad <shahd.riad2344@gmail.com>
2023-10-08 17:30:03 -0700

    lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	    for(char alpha ='a' ;alpha <= 'z' ;alpha++)
+		    putchar(alpha);
+	        putchar('\n');
+		    return (0);
+}
