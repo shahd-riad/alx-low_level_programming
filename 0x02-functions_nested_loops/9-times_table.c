54cb511ed421ae694258e5a2d6dc0f353faf91bf
shahd-riad <shahd.riad2344@gmail.com>
2023-10-10 19:41:45 -0700

    edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,15 +10,16 @@ void times_table(void)
 {
 int num;	
 int i;
+int result;
 
 	for (num = 0; num < 10; num++)
 	{
-		for(i = 0; i < 10; i++)
+		for (i = 0; i < 10; i++)
 		{
-			int result = num * i;										                printf("%d", result);
-			if(i < 9)
-				printf(", ");									
-	       	}
-	        putchar('\n');
+			result = num * i;										                printf("%d", result);
+		if (i < 9)
+			printf(",\t");									
+		}
+		putchar('\n');
 	}
 }
