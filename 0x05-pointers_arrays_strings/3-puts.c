e75f86bb17e74ec440227c617c5bbbbffa59a1f3
shahd-riad <shahd.riad2344@gmail.com>
2023-10-16 17:31:12 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *  * _puts - prints a string, to stdout
+ *   * @str: value to evaluate.
+ *    * Return: void
+ *     */
+
+void _puts(char *str)
+{
+		
+
+			while (*str != '\0')
+					{
+								_putchar(*str);
+									*str++;
+											}
+			_putchar('\n');
+}
