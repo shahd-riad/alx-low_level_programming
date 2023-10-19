2752cf9bce9c7740093e5755ae0c37bf5544fce4
shahd-riad <shahd.riad2344@gmail.com>
2023-10-19 07:03:35 -0700

    stlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,21 +4,19 @@
 /**
  *  * _strlen - returns the length of a string.
  *   * by traversing throughout the values of string s
- *    * as it increments variable length and stops the loop at the end of
- *     *the string with len incremented to fit the length of the string
+ *    * as it increments variable length and stops the loop at the end of the string 
+ *     * with len incremented to fit the length of the string
  *      * @s: value to evaluate.
  *       * Return: len
 */
 
 int _strlen(char *s)
 {
-	int len;
+	int len = 0;
 
-	len = 0;
-		while (*s != '\0')
-		{		
-			s++;
-			len++;
-		}
-		return (len);
+	while (*s != '\0')
+	{
+		s++;
+		len++;											}
+	return (len);
 }
