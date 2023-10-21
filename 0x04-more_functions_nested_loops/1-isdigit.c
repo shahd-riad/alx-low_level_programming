bc357b6d3b4ce68850802f68c9538e1b7a7d158b
shahd-riad <shahd.riad2344@gmail.com>
2023-10-21 14:23:38 -0700

    Check fail

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,9 +7,8 @@
 */
 int _isdigit(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-		return (0);
-	else if (c % 10)
+	if ((c >= 48) && (c <= 57))
 		return (1);
+	else
+		return (0);
 }
-
