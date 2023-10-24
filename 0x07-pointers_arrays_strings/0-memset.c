95c57e663565b63214480015e995a982949f0f80
shahd-riad <shahd.riad2344@gmail.com>
2023-10-23 17:42:42 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,13 +15,9 @@ char *_memset(char *s, char b, unsigned int n)
 
 	sptr = s;
 	
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
-		if (i == n)
-		{
-			s[i] = '\0';
-		}
 	}
 	return (sptr);
 }
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers 3rd
