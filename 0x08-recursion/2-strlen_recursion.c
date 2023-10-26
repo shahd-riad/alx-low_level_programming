bfdd33eb10206817538044a4f81b4eb5abd6d7c3
shahd-riad <shahd.riad2344@gmail.com>
2023-10-25 18:40:30 -0700

    LEN

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,10 @@
 */
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	i = 0;
     	if (*s)
 	{
 		return 1 + _strlen_recursion(s + 1);
     	}
-	else if (s == '\0')
+	else if (*s == '\0')
 		return (0);
 }
