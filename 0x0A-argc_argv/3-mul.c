6917982b71a3b149b9e438b80d1bc027d0be585b
shahd-riad <shahd.riad2344@gmail.com>
2023-10-27 16:04:32 -0700

    args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,10 +16,10 @@ int main(int argc, char **argv)
 	mul = 0;
 	if (argc < 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
-	mul = atoi(argv[1]) * atoi(argv[2]);
+	mul = (atoi(argv[1])) * (atoi(argv[2]));
 	printf("%d\n", mul);
 return (0);
 }
