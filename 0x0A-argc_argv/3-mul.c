67e95470b4fc554114fd345c31a9ca54f237d6ca
shahd-riad <shahd.riad2344@gmail.com>
2023-10-27 18:09:04 -0700

    frnj

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,21 @@
  * Return: If the program receives two arguments - 0.
  * If the program does not receive two arguments - 1.
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int mul;
-	
-	mul = 0;
-	if (argc !=  3)
+	int num1, num2, prod;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	mul = (atoi(argv[1])) * (atoi(argv[2]));
-	printf("%d\n", mul);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
 return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 return (0);
