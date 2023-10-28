8ff6d1d687892dd9084497a3c2b8089183ee2157
shahd-riad <shahd.riad2344@gmail.com>
2023-10-28 02:49:26 -0700

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,21 +10,25 @@
  * Return: If one of the numbers contains symbols that are non-digits - 1.
  * Otherwise - 0.
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i;
-	int sum;
+	int num, digit, sum = 0;
 
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	for (num = 1; num < argc; num++)
 	{
-		if (atoi(argv[i]) == 0)
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += atoi(argv[i]);
+
+		sum += atoi(argv[num]);
 	}
+
 	printf("%d\n", sum);
-return (0);
+
+	return (0);
 }
