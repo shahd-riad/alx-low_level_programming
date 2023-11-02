caae3915efa5739771a1cbf809587caafae5eeee
shahd-riad <shahd.riad2344@gmail.com>
2023-11-01 19:38:48 -0700

    kk

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
+ *
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ * Otherwise - a pointer to an array of strings (words).
+*/
+char **strtow(char *str)
+{
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+}
