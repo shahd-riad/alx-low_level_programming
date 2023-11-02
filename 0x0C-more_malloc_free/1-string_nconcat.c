c7553ba701a49bbf8407fa6c7f0f66af5324c8ba
shahd-riad <shahd.riad2344@gmail.com>
2023-11-02 12:41:43 -0700

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,14 +14,12 @@ int _strlen(char *s);
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {	
-	int len_s1;
-	int len_s2;
-	int i;
-	int j = 0;
+	unsigned int len_s1;
+	unsigned int i;
+	unsigned int j = 0;
 	char *s3;
 
 	len_s1 = (s1 == NULL) ? 0 : _strlen(s1);
-	len_s2 = (s2 == NULL) ? 0 : _strlen(s2);
 
 	s3 = malloc(sizeof(char) * (len_s1 + n + 1));
 
