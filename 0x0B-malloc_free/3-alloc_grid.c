43e56e2028d62665555b11963e6dc3ffe28f1f78
shahd-riad <shahd.riad2344@gmail.com>
2023-11-01 19:10:37 -0700

    mand

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
  * Return: pointer to the created array
  * If width or height is 0 or negative, return NULL
  * The function should return NULL on failure
- */
+*/
 int **alloc_grid(int width, int height)
 {
 	int **grid;
@@ -28,19 +28,22 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(sizeof(int) * width);
-		if (grid == NULL)
-		 {
-			 for (i = 0; i < height; i++)
-				 free(grid[i]);
-		 }
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+
+			free(grid);
+			return (NULL);
+		}
 	}
+
 	/* initialize all values in the array to 0 */
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
-
 
 	return (grid);
 }
