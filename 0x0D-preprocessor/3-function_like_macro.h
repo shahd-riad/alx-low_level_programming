c7afb33c6cb8ed74d6c45cb273a7cd21e62c9e6c
shahd-riad <shahd.riad2344@gmail.com>
2023-11-06 18:06:45 -0800

    abs

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,5 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#include <stdlib.h>
 /*
  * File: 3-function_like_macro.h
  *
@@ -8,6 +7,6 @@
  * that computes the absolute value of a number x.
 */
 
-#define ABS(x) abs(x)
+#define ABS(x)  ((x) < 0 ? (-1 * x) : (x))
 
 #endif
