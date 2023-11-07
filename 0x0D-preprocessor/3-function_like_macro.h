52f9860f2dc11a5ee5b8fccb1cf8e546d59d102f
shahd-riad <shahd.riad2344@gmail.com>
2023-11-06 18:00:50 -0800

    pre

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#include <stdlib.h>
+/*
+ * File: 3-function_like_macro.h
+ *
+ * Desc: Header file that defines a function-like macro ABS(x)
+ * that computes the absolute value of a number x.
+*/
+
+#define ABS(x) abs(x)
+
+#endif
