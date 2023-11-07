b1ba08ae01ed237bb986af5dccb1ba663ae69854
shahd-riad <shahd.riad2344@gmail.com>
2023-11-06 19:52:18 -0800

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,19 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
+
+	if ((*d).name == NULL)
 		printf("Name: (nil)\n");
 	else
-		printf("Name: %s\n", d->name);
-	if (d->age == NULL)
+		printf("Name: %s\n", (*d).name);
+
+	if ((*d).age < 0)
 		printf("Age: (nil)\n");
 	else
-		printf("Age: %s\n", d->age);
-	if (d->owner == NULL)
+		printf("Age: %f\n", (*d).age);
+
+	if ((*d).owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", (*d).owner);
 }
