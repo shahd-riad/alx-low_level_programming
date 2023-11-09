7a76e701f5080b94ad154277eb781dd2b4d1eb8d
shahd-riad <shahd.riad2344@gmail.com>
2023-11-08 19:57:18 -0800

    gdgdg

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,35 +8,39 @@
  *
  * Return: Always 0.
 */
-void print_opcodes(char *a, int n);
-
 int main(int argc, char *argv[])
 {
-	int arg;
-	
-	arg = atoi(argv[1]);
-       	if (argc != 2)
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (arg < 0)
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	print_opcodes((char *)main, arg);
-	return (0);
-}
 
-void print_opcodes(char *a, int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
+	for (index = 0; index < bytes; index++)
 	{
-		printf("%.2x ", a[i]);
-		if (i < n - 1)
-			printf(" ");
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
 	}
+
 	printf("\n");
+
+	return (0);
 }
