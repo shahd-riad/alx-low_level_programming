485f1ab207e4f929ad6e2814111bbbbed23d75bb
shahd-riad <shahd.riad2344@gmail.com>
2023-11-16 17:46:33 -0800

    adv

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *new_str;
-	int len;
+	unsigned int len = 0;
 
 	while (str)
 		len++;
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,33 +7,30 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t last = *head;
-	char *new_str;
-	int len;
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	while (str)
+	while (str[len])
 		len++;
 
-	new_str = strdup(str);
-	new_node = malloc(sizeof(list_t));
-
-	if (!new_node)
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	new_node->str = new_str;
-	new_node->len = len;
-	new_node->next = NULL;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new_node;
-		return (new_node);
+		*head = new;
+		return (new);
 	}
 
-	while (last->next != NULL)
-		last = last->next;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
 
-	last->next = new_node;
-	return (*new_node);
+	return (new);
 }
