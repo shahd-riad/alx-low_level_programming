218bc622f1c45509bfa4c73b1e4a1946ca2d4f91
shahd-riad <shahd.riad2344@gmail.com>
2023-11-16 18:12:50 -0800

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-section .data
-    hello db 'Hello, Holberton', 0   ; null-terminated string
-
-section .text
-    global main
-    extern printf
-
+global    main
+        extern    printf
 main:
-    ; Call printf with the address of the format string
-    mov rdi, hello
-    call printf
-
-    ; Exit the program
-    mov rax, 60         ; syscall: exit
-    xor rdi, rdi        ; status: 0
-    syscall
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
